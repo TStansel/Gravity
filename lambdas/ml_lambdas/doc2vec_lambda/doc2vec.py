b2b28999cafa90f4d58d7c8f4c2a0eadfe120b43
Mason Spong <mason@Masons-MacBook-Air.local>
2021-11-23 12:26:46 -0700

    update doc2vec_lambda to work with docker

--- lambdas/ml_lambdas/doc2vec_lambda/doc2vec.py
+++ lambdas/ml_lambdas/doc2vec_lambda/doc2vec.py
@@ -1,9 +1,10 @@
 import gensim
+import json
 
-model = gensim.models.doc2vec.Doc2Vec.load('model/so_top_100k_vec_50_win_5_min_2_ep_80.pkl')
+model = gensim.models.doc2vec.Doc2Vec.load('so_top_100k_vec_50_win_5_min_2_ep_80.pkl')
 
 def lambda_handler(event=None, context=None):
-  return model.infer_vector(string_to_tokens(event["raw_text"]))
+  return json.dumps({"vector": model.infer_vector(string_to_tokens(event["raw_text"])).tolist()})
 
 def string_to_tokens(string):
     return gensim.utils.simple_preprocess(string)
