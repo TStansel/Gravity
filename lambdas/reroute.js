f5f8f0bd2066cb807a7325ff1086b856ee909465
Thomas Stansel <thomas.s.stansel@gmail.com>
2021-11-30 16:31:30 -0700

    reroute

--- lambdas/reroute.js
+++ lambdas/reroute.js
@@ -1,4 +1,5 @@
 const axios = require('axios');
+import {v4 as uuidv4} from 'uuid';
 
 exports.handler = async (event) => {
     console.log('Request event: ', event);
@@ -12,39 +13,269 @@ exports.handler = async (event) => {
     
     switch(true) {
         case eventType === 'message' && eventSubtype === undefined:{
+            // Someone posted a message
             
             response = buildResponse(200,event);
-            
+
             let messageEvent = event.event;
             let messageText = messageEvent.text;
             let channelID = messageEvent.channel;
             let messageID = messageEvent.ts;
             
-            let params = {
-              messages: [{
-                  text:messageText,
-                  id: messageID,
-                  channelID: channelID
-              }],
-            };
+            // Check if the message is in a thread or has a file attached
+            if(!messageEvent.hasOwnProperty('thread_ts') || messageEvent.ts === messageEvent.thread_ts){
+                
+                let params = {
+                    messages: [{
+                        text:messageText,
+                        id: messageID,
+                        channelID: channelID
+                    }],
+                };
             
-            let config = {
-              method: 'post',
-              url: 'https://a3rodogiwi.execute-api.us-east-2.amazonaws.com/Staging/nlp',
-              data: params
-            };
-            
-            const res = await axios(config);
-            console.log('NLP: ', res)
-            if(res.data.body.length === 0){
-                // Send question to similiar question lambda
-                let question = res.data.body[0];
+                let config = {
+                    method: 'post',
+                    url: 'https://a3rodogiwi.execute-api.us-east-2.amazonaws.com/Staging/nlp',
+                    data: params
+                };
+                
+                const res = await axios(config);
+                console.log('NLP: ', res.data.body)
+                
+                // Check if the message is a question
+                if(res.data.body.length === 1){
+                    let question = res.data.body[0];
+                    /*
+                    let config = {
+                        method: 'get',
+                        url: 'https://a3rodogiwi.execute-api.us-east-2.amazonaws.com/Staging/dbcalls?query=show tables',
+                    };
+                
+                    const dbRes = await axios(config);
+                    console.log('Db Call: ', dbRes)*/
+                    
+                    // TODO: Send question to similiar question lambda
+
+                    // TODO: Use result of similiar question lambda to build message to send to user
+                
+                    // Probably better to build 3 messages and send them so we can delete them if the dismiss button is pressed
+                    
+                    /* Commented code below grabs the link for a message given the channelID and message ts
+                    // channelID should be the same since we should only be pulling questions from this same channel
+                    // let messageTS = // TS of answer message from DB
+                    
+                    let linkConfig = {
+                        method: 'get',
+                        url: 'https://slack.com/api/chat.getPermalink?channel='+channelID+'&message_ts='+messageTS,
+                        headers: {
+                            'Authorization': 'Bearer xoxb-2516673192850-2728955403541-DIAuQAWa2QhauF13bgerQYnK',
+                            'Content-Type': 'application/json'
+                        },
+                    };
+                    
+                    const linkRes = await axios(linkConfig);
+                    console.log("link: ", linkRes);
+                    */
+                    
+                    let msgParams = {
+                        "channel": "C02K2H9SWG6",
+                        "user": "U02FK8XA2RX",
+                        "text": "Hello World",
+                        "blocks": [
+                            {
+                                "type": "section",
+                                "text": {
+                                    "type": "mrkdwn",
+                                    "text": "Hello"
+                                }
+                            },
+                            {
+                                "type": "divider"
+                            },
+                            {
+                                "type": "section",
+                                "text": {
+                                    "type": "mrkdwn",
+                                    "text": "Suggestion 1"
+                                }
+                            },
+                            {
+                                "type": "actions",
+                                "elements": [
+                                    {
+                                        "type": "button",
+                                        "text": {
+                                            "type": "plain_text",
+                                            "text": "Helpful"
+                                        },
+                                        "value": "helpful",
+                                        "action_id": "1_helpful"
+                                    },
+                                    {
+                                        "type": "button",
+                                        "text": {
+                                            "type": "plain_text",
+                                            "text": "Not Helpful"
+                                        },
+                                    "value": "notHelpful",
+                                    "action_id": "1_nothelpful"
+                                    },
+                                    {
+                                        "type": "button",
+                                        "style": "danger",
+                                        "text": {
+                                            "type": "plain_text",
+                                            "text": "Dismiss"
+                                        },
+                                        "value": "dismiss",
+                                        "action_id": "1_dismiss"
+                                    }
+                                ]
+                            },
+                            {
+                                "type": "section",
+                                "text": {
+                                    "type": "mrkdwn",
+                                    "text": "Suggestion 2"
+                                }
+                            },
+                            {
+                                "type": "actions",
+                                "elements": [
+                                    {
+                                        "type": "button",
+                                        "text": {
+                                            "type": "plain_text",
+                                            "text": "Helpful"
+                                        },
+                                        "value": "helpful",
+                                        "action_id": "2_helpful"
+                                    },
+                                    {
+                                        "type": "button",
+                                        "text": {
+                                            "type": "plain_text",
+                                            "text": "Not Helpful"
+                                        },
+                                        "value": "notHelpful",
+                                        "action_id": "2_nothelpful"
+                                    },
+                                    {
+                                        "type": "button",
+                                        "style": "danger",
+                                        "text": {
+                                            "type": "plain_text",
+                                            "text": "Dismiss"
+                                        },
+                                        "value": "dismiss",
+                                        "action_id": "2_dismiss"
+                                    }
+                                ]
+                            },
+                            {
+                                "type": "section",
+                                "text": {
+                                    "type": "mrkdwn",
+                                    "text": "Suggestion 3"
+                                }
+                            },
+                            {
+                                "type": "actions",
+                                "elements": [
+                                    {
+                                        "type": "button",
+                                        "text": {
+                                            "type": "plain_text",
+                                            "text": "Helpful"
+                                        },
+                                        "value": "helpful",
+                                        "action_id": "3_helpful"
+                                    },
+                                    {
+                                        "type": "button",
+                                        "text": {
+                                            "type": "plain_text",
+                                            "text": "Not Helpful"
+                                        },
+                                        "value": "notHelpful",
+                                        "action_id": "3_nothelpful"
+                                    },
+                                    {
+                                        "type": "button",
+                                        "style": "danger",
+                                        "text": {
+                                            "type": "plain_text",
+                                            "text": "Dismiss"
+                                        },
+                                        "value": "dismiss",
+                                        "action_id": "3_dismiss"
+                                    }
+                                ]
+                            }
+                        ]
+                    }
+                    let msgConfig = {
+                        method: 'post',
+                        url: 'https://slack.com/api/chat.postEphemeral',
+                        headers: {
+                            'Authorization': 'Bearer xoxb-2516673192850-2728955403541-DIAuQAWa2QhauF13bgerQYnK',
+                            'Content-Type': 'application/json'
+                        },
+                        data: msgParams
+                    };
+                    const msgRes = await axios(msgConfig);
+                    console.log("Message Sent: ",msgRes);
+                
+                }
+                
             }
+            
+            
             break;
         }
         case eventType === 'message' && eventSubtype === 'channel_join':{
-            
+            // Bot was added to a channel
             response = buildResponse(200,event);
+            
+            // Check if the Workspace is already in the DB
+            let teamID = event.authorizations.team_id;
+            let db1Config = {
+                method: 'get',
+                url: `https://a3rodogiwi.execute-api.us-east-2.amazonaws.com/Staging/dbcalls?query=
+                select * from SlackWorkspace 
+                where WorkspaceID =`+teamID,
+            };
+                
+            const dbRes = await axios(db1Config);
+            if(dbRes.data.length > 0){
+                
+            }
+            console.log('Db Call: ', dbRes)
+            // Start Adding new workspace
+            
+            
+            let teamConfig = {
+                method: 'get',
+                url: 'https://slack.com/api/team.info?team='+teamID,
+                headers: {
+                    'Authorization': 'Bearer xoxb-2516673192850-2728955403541-DIAuQAWa2QhauF13bgerQYnK',
+                    'Content-Type': 'application/json'
+                },
+            }
+            const teamRes = await axios(config);
+            let teamName = teamRes.team.name;
+            
+            let uuid = uuidv4();
+            
+            let dbConfig = {
+                method: 'get',
+                url: `https://a3rodogiwi.execute-api.us-east-2.amazonaws.com/Staging/dbcalls?query=
+                insert into SlackWorkspace (SlackWorkspaceID, WorkspaceID, Name)
+                values (`+uuid+','+teamID+','+teamName+')',
+            };
+                
+            const dbRes = await axios(dbConfig);
+            console.log('Db Call: ', dbRes)
             
             let messageEvent = event.event;
             let channelID = messageEvent.channel;
@@ -53,12 +284,12 @@ exports.handler = async (event) => {
                 method: 'get',
                 url: 'https://slack.com/api/conversations.history?channel='+channelID+'&limit=100000',
                 headers: {
-                    'token': 'xoxb-2516673192850-2728955403541-DIAuQAWa2QhauF13bgerQYnK',
                     'Authorization': 'Bearer xoxb-2516673192850-2728955403541-DIAuQAWa2QhauF13bgerQYnK',
                     'Content-Type': 'application/json'
                 },
             };
             
+            // Grab the 100,000 most recent messages
             const res = await axios(config);
             
             let messages = res.data.messages;
@@ -83,11 +314,50 @@ exports.handler = async (event) => {
               url: 'https://a3rodogiwi.execute-api.us-east-2.amazonaws.com/Staging/nlp',
               data: params
             };
+            // See if each cleaned message is a question
             const nlpRes = await axios(nlpConfig);
             console.log('NLP: ', nlpRes)
             if(nlpRes.data.body.length > 0){
                 let questions = nlpRes.data.body;
-                // Send each question (and the associated thread_ts and channelID) to the DB
+                // TODO: Send each question (and the associated thread_ts and channelID) to the DB
+                console.log("nlpRes app added: ",nlpRes.data.body)
+                for (let i = 0; i < nlpRes.data.body.length; i++){
+                    let messageTS = nlpRes.data.body[i].thread_ts;
+                    let answerConfig = {
+                        method: 'get',
+                        url: 'https://slack.com/api/conversations.replies?channel='+channelID+'&ts='+messageTS,
+                        headers: {
+                            'Authorization': 'Bearer xoxb-2516673192850-2728955403541-DIAuQAWa2QhauF13bgerQYnK',
+                            'Content-Type': 'application/json'
+                        },
+                    };
+                    
+                    const answerRes = await axios(answerConfig);
+                    console.log("Answer Result: ", answerRes);
+                    
+                    const answerTS = answerRes.data.messages[1].ts
+                    
+                    let linkConfig = {
+                        method: 'get',
+                        url: 'https://slack.com/api/chat.getPermalink?channel='+channelID+'&message_ts='+answerTS,
+                        headers: {
+                            'Authorization': 'Bearer xoxb-2516673192850-2728955403541-DIAuQAWa2QhauF13bgerQYnK',
+                            'Content-Type': 'application/json'
+                        },
+                    };
+                    
+                    const linkRes = await axios(linkConfig);
+                    console.log("link: ", linkRes);
+                    
+                    let config = {
+                        method: 'get',
+                        url: 'https://a3rodogiwi.execute-api.us-east-2.amazonaws.com/Staging/dbcalls?query=show tables',
+                    };
+                
+                    const dbRes = await axios(config);
+                    console.log('Db Call: ', dbRes)
+                    
+                }
             }
             
             break;
@@ -115,3 +385,4 @@ function buildResponse(statusCode, event) {
     }
   
 }
+
