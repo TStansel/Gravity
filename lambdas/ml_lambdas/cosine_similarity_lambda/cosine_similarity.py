11e660c8502cf547fd4cd092cf2c7bf66df01a85
Mason Spong <mason@Masons-MacBook-Air.local>
2021-12-02 13:34:54 -0700

    cosine_similarity changes

--- lambdas/ml_lambdas/cosine_similarity_lambda/cosine_similarity.py
+++ lambdas/ml_lambdas/cosine_similarity_lambda/cosine_similarity.py
@@ -6,7 +6,7 @@ def lambda_handler(event=None, context=None):
   new_question = event["payload"]["new_question"]
   old_questions = event["payload"]["old_questions"]
   
-  similarities = np.array(list(map(lambda old_question: cosine_similarity(new_question, old_question["TextVector"]), old_questions)))
+  similarities = np.array(list(map(lambda old_question: cosine_similarity(new_question, json.loads(old_question["TextVector"])["vector"]), old_questions)))
   top_10_idx = similarities.argsort()[-10:][::-1]
 
   return json.dumps(list(map(lambda idx: {"QuestionID": old_questions[idx]["QuestionID"], "similarity": similarities[idx]}, top_10_idx)))
