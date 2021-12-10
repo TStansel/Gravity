1376dc3f074b5125f2e876ae341d99ee9530dcd3
Thomas Stansel <thomas.s.stansel@gmail.com>
2021-12-10 13:52:55 -0700

    changes

--- lambdas/ml_lambdas/doc2vec_lambda/doc2vec.py
+++ lambdas/ml_lambdas/doc2vec_lambda/doc2vec.py
@@ -4,9 +4,8 @@ import json
 model = gensim.models.doc2vec.Doc2Vec.load('so_top_100k_vec_50_win_5_min_2_ep_80.pkl')
 
 def lambda_handler(event=None, context=None):
-  event = event["payload"]
+  print("Request Event", event)
   return json.dumps({"vector": model.infer_vector(string_to_tokens(event["text"])).tolist()})
 
 def string_to_tokens(string):
     return gensim.utils.simple_preprocess(string)
-
