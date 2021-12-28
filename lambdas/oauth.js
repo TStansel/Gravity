219a78ec7c8afecfab38da22ce2f84da6674bca7
Thomas Stansel <thomas.s.stansel@gmail.com>
2021-12-28 10:02:54 -0700

    Tokens are stored in db and Workspace is added after authentication

--- lambdas/oauth.js
+++ lambdas/oauth.js
@@ -1,5 +1,12 @@
-const axios = require('axios')
-const qs = require('qs')
+const axios = require('axios');
+const qs = require('qs');
+const { v4: uuidv4 } = require("uuid");
+const data = require("data-api-client")({
+    secretArn:
+      "arn:aws:secretsmanager:us-east-2:579534454884:secret:rds-db-credentials/cluster-4QWLO4T4HOH5I2B5367KESUM5Y/admin-lplDgu",
+    resourceArn: "arn:aws:rds:us-east-2:579534454884:cluster:osmosix-db-cluster",
+    database: "osmosix", // set a default database
+  });
 
 exports.handler = async (event) => {
     console.log(event)
@@ -15,15 +22,31 @@ exports.handler = async (event) => {
         client_secret: clientSecret
     };
 
-      let url = "https://slack.com/api/oauth.v2.access"
-      const oauthRes = await axios.post(url,qs.stringify(oauthParams));
+    let url = "https://slack.com/api/oauth.v2.access"
+    const oauthRes = await axios.post(url,qs.stringify(oauthParams));
       
-      let botToken = oauthRes.data.access_token;
-      let userToken = oauthRes.data.authed_user.access_token;
-      let workspaceID = oauthRes.data.team.id;
-      let workspaceName = oauthRes.data.team.name;
+    let botToken = oauthRes.data.access_token;
+    let userToken = oauthRes.data.authed_user.access_token;
+    let workspaceID = oauthRes.data.team.id;
+    let workspaceName = oauthRes.data.team.name;
+    let workspaceUUID = uuidv4();
 
-      // Need to store these and associate them with the workspace we were just installed in.
-      
+    let insertWorkspaceSql =
+        `insert into SlackWorkspace (SlackWorkspaceUUID, WorkspaceID, Name) values (:SlackWorkspaceUUID, :WorkspaceID, :Name)`;
+    
+    let insertWorkspaceResult = await data.query(insertWorkspaceSql, {
+        SlackWorkspaceUUID: workspaceUUID,
+        WorkspaceID: workspaceID,
+        Name: workspaceName
+    });
 
+    let insertTokenSql =
+        `insert into SlackToken (SlackTokenUUID, SlackWorkspaceUUID, BotToken, UserToken) values (:SlackTokenUUID, :SlackWorkspaceUUID, :BotToken, :UserToken)`;
+    
+    let insertTokenResult = await data.query(insertTokenSql, {
+        SlackTokenUUID: uuidv4(),
+        SlackWorkspaceUUID: workspaceUUID,
+        BotToken: botToken,
+        UserToken: userToken
+    });
 };
