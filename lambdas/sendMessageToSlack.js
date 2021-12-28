82f91f34c271e66b7c23e40ff65eeeae5bffc43e
Thomas Stansel <thomas.s.stansel@gmail.com>
2021-12-28 11:20:09 -0700

    Fixed bug where if there were no suggestions for a siiliar question the sendMessageToSlack would error out

--- lambdas/sendMessageToSlack.js
+++ lambdas/sendMessageToSlack.js
@@ -11,16 +11,8 @@ const data = require("data-api-client")({
 exports.handler = async (event) => {
 
     event = event.payload;
-    
-    let mostSimilarQuestion = parseJson(event.questions)[0];
-    let mostSimilarQuestionUUID = mostSimilarQuestion.SlackQuestionID;
-    
-    let getQuestionSql =
-      "select SlackAnswerUUID from SlackQuestion where SlackQuestionUUID = :SlackQuestionUUID";
-    
-    let getQuestionResult = await data.query(getQuestionSql, {
-      SlackQuestionUUID: mostSimilarQuestionUUID,
-    });
+
+    let mostSimiliarQuestions = parseJson(event.questions);
 
     let getBotTokenSql =
         `select SlackToken.BotToken from SlackToken 
@@ -32,6 +24,37 @@ exports.handler = async (event) => {
     });
 
     let botToken = getBotTokenResult.records[0].BotToken;
+
+    if(mostSimiliarQuestions.length === 0){
+
+        let noSuggestionsMessageParams = {
+            "channel": event.channelID,
+            "user": event.userID,
+            "text": "We currently don't have an answer for your question. After someone answers your question make sure to mark it as an answer so next time someone has a question similiar to yours we can help them out!",
+        };
+
+        let msgConfig = {
+            method: 'post',
+            url: 'https://slack.com/api/chat.postEphemeral',
+            headers: {
+                'Authorization': 'Bearer ' + botToken,
+                'Content-Type': 'application/json'
+            },
+            data: noSuggestionsMessageParams
+        };
+        const msgRes = await axios(msgConfig);
+        return msgRes.data;
+    }
+    
+    let mostSimilarQuestion = mostSimiliarQuestions[0];
+    let mostSimilarQuestionUUID = mostSimilarQuestion.SlackQuestionID;
+    
+    let getQuestionSql =
+      "select SlackAnswerUUID from SlackQuestion where SlackQuestionUUID = :SlackQuestionUUID";
+    
+    let getQuestionResult = await data.query(getQuestionSql, {
+      SlackQuestionUUID: mostSimilarQuestionUUID,
+    });
     
     let answerLink;
     let isAnswerInDb = false;
