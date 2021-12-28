6d185c86d117729e8ba80f8ac3763e217287a7a7
Thomas Stansel <thomas.s.stansel@gmail.com>
2021-12-28 10:36:48 -0700

    commented out all console.logs and no longer hard coding token

--- lambdas/preliminaryAppAddedWork.js
+++ lambdas/preliminaryAppAddedWork.js
@@ -8,14 +8,13 @@ const data = require("data-api-client")({
 });
 
 exports.handler = async (event) => {
-  // TODO: Add auth for axios slack calls
   // TODO: Send a message when this case starts and ends to let users know that the app is working on adding the history and when it is finished
 
   // Bot was added to a channel
-  console.log(event);
+  //console.log(event);
 
   // Check if the Workspace is already in the DB
-  console.log("check if workspace is in DB");
+  //console.log("check if workspace is in DB");
 
   let getWorkspaceSql =
     "select * from SlackWorkspace where WorkspaceID = :workspaceID";
@@ -25,7 +24,18 @@ exports.handler = async (event) => {
     workspaceID: workspaceID,
   });
 
-  console.log("getWorkspaceresult: ", getWorkspaceResult);
+  //console.log("getWorkspaceresult: ", getWorkspaceResult);
+
+  let getBotTokenSql =
+    `select SlackToken.BotToken from SlackToken 
+    join SlackWorkspace on SlackToken.SlackWorkspaceUUID = SlackWorkspace.SlackWorkspaceUUID 
+    where SlackWorkspace.WorkspaceID = :workspaceID`;
+
+  let getBotTokenResult = await data.query(getBotTokenSql, {
+    workspaceID: workspaceID,
+  });
+
+  let botToken = getBotTokenResult.records[0].BotToken;
 
   let workspaceUUID;
 
@@ -33,14 +43,14 @@ exports.handler = async (event) => {
   if (getWorkspaceResult.records.length === 0) {
     // Workspace does not exist, get slack channel
 
-    console.log("calling slack api to get workspace name");
+    //console.log("calling slack api to get workspace name");
 
     let getSlackWorkspaceNameConfig = {
       method: "get",
       url: "https://slack.com/api/team.info?team=" + workspaceID,
       headers: {
         Authorization:
-          "Bearer xoxb-2516673192850-2728955403541-DIAuQAWa2QhauF13bgerQYnK", // TODO: stop this from being hard-coded
+          "Bearer " + botToken,
         "Content-Type": "application/json",
       },
     };
@@ -49,7 +59,7 @@ exports.handler = async (event) => {
       getSlackWorkspaceNameConfig
     );
 
-    console.log("Get Workspace Name Call:", getSlackWorkspaceNameResult);
+    //console.log("Get Workspace Name Call:", getSlackWorkspaceNameResult);
 
     // Add workspace to DB
 
@@ -65,14 +75,14 @@ exports.handler = async (event) => {
       Name: workspaceName,
     });
 
-    console.log("insertWorkspaceResult: ", insertWorkspaceResult);
+    //console.log("insertWorkspaceResult: ", insertWorkspaceResult);
   } else {
     // get UUID from get Call to be used in SlackChannel Creation
     workspaceUUID = getWorkspaceResult.records[0].SlackWorkspaceUUID;
   }
 
   // Check if slack channel exists in DB
-  console.log("check if slack channel is in DB");
+  //console.log("check if slack channel is in DB");
 
   let channelID = event.payload.channelID;
 
@@ -84,19 +94,20 @@ exports.handler = async (event) => {
     channelID: channelID,
   });
 
-  console.log("getChannelResult: ", getChannelResult);
+  //console.log("getChannelResult: ", getChannelResult);
 
   let channelUUID;
 
   // If the channel already exists skip the steps of putting all channel users in DB
   if (getChannelResult.records.length > 0) {
-    console.log("Channel already exists in DB (unexpected)");
+    //console.log("Channel already exists in DB (unexpected)");
+
     channelUUID = getChannelResult.records[0].SlackChannelUUID;
     return { channelID: channelID, channelUUID: channelUUID };
   }
 
   // Slack channel does not exist in DB
-  console.log("Channel does not exist in DB");
+  //console.log("Channel does not exist in DB");
 
   channelUUID = uuidv4();
 
@@ -106,13 +117,13 @@ exports.handler = async (event) => {
     url: "https://slack.com/api/conversations.info?channel=" + channelID,
     headers: {
       Authorization:
-        "Bearer xoxb-2516673192850-2728955403541-DIAuQAWa2QhauF13bgerQYnK", // TODO: don't hard code this
+        "Bearer " + botToken,
       "Content-Type": "application/json",
     },
   };
 
   const getChannelInfoResult = await axios(getChannelInfoConfig);
-  console.log("Get Channel Info Call:", getChannelInfoResult);
+  //console.log("Get Channel Info Call:", getChannelInfoResult);
 
   // Insert channel into DB
   let channelName = getChannelInfoResult.data.channel.name;
@@ -127,10 +138,10 @@ exports.handler = async (event) => {
     channelName: channelName,
   });
 
-  console.log("insertChannelResult: ", insertChannelResult);
+  //console.log("insertChannelResult: ", insertChannelResult);
 
   // Because this is a new channel we need to add all users into the DB if they dont exist
-  console.log("start getting all users in channel by pagination");
+  //console.log("start getting all users in channel by pagination");
   let cursor = null;
   let channelMembers = [];
 
@@ -153,14 +164,14 @@ exports.handler = async (event) => {
         cursorParam,
       headers: {
         Authorization:
-          "Bearer xoxb-2516673192850-2728955403541-DIAuQAWa2QhauF13bgerQYnK", // TODO: don't hardcode
+          "Bearer " + botToken,
         "Content-Type": "application/json",
       },
     };
 
     const getChannelUsersResult = await axios(getChannelUsersConfig);
 
-    console.log("Get Channel Users Call:", getChannelUsersResult);
+    //console.log("Get Channel Users Call:", getChannelUsersResult);
 
     channelMembers = channelMembers.concat(getChannelUsersResult.data.members);
 
@@ -170,19 +181,19 @@ exports.handler = async (event) => {
       getChannelUsersResult.data.response_metadata.next_cursor === ""
     ) {
       // Response has no next_cursor property set so we are done paginating!
-      console.log("no cursor in response, done paginating");
+      //console.log("no cursor in response, done paginating");
       cursor = null;
     } else {
       cursor = getChannelUsersResult.data.response_metadata.next_cursor.replace(
         /=/g,
         "%3D"
       );
-      console.log("cursor found in result, encoding and paginating");
+      //console.log("cursor found in result, encoding and paginating");
     }
   } while (cursor !== null); // When done paginating cursor will be set to null
 
   // Now get all users from the workspace in the DB in order to add new users
-  console.log("Get all users in workspace");
+  //console.log("Get all users in workspace");
 
   let getWorkspaceUsersSql =
     "select SlackID from SlackUser where SlackWorkspaceUUID = :workspaceUUID";
@@ -191,13 +202,13 @@ exports.handler = async (event) => {
     workspaceUUID: workspaceUUID,
   });
 
-  console.log("getWorkspaceUsersResult: ", getWorkspaceUsersResult);
+  //console.log("getWorkspaceUsersResult: ", getWorkspaceUsersResult);
 
   let slackUserIdSet = new Set();
   for (let row of getWorkspaceUsersResult.records) {
     slackUserIdSet.add(row.SlackID);
   }
-  console.log(slackUserIdSet);
+  //console.log(slackUserIdSet);
 
   let membersNotInDB = [];
   for (let slackID of channelMembers) {
@@ -206,16 +217,16 @@ exports.handler = async (event) => {
     }
   }
 
-  console.log("channelMembers: ", channelMembers);
-  console.log("membersNotInDB: ", membersNotInDB);
+  //console.log("channelMembers: ", channelMembers);
+  //console.log("membersNotInDB: ", membersNotInDB);
 
   if (membersNotInDB.length === 0) {
-    console.log("no new users to add to DB, returning");
+    //console.log("no new users to add to DB, returning");
     return { channelID: channelID, channelUUID: channelUUID };
   }
 
   // Now add these new users to SlackUser in DB
-  console.log("trying to add new SlackUsers");
+  //console.log("trying to add new SlackUsers");
 
   let batchInsertNewSlackUserSql =
     "insert into SlackUser (SlackUserUUID, SlackWorkspaceUUID, SlackID) values (:slackUserUUID, :workspaceUUID, :slackID)";
@@ -225,14 +236,14 @@ exports.handler = async (event) => {
     { slackUserUUID: uuidv4(), workspaceUUID: workspaceUUID, slackID: slackID },
   ]);
 
-  console.log("batchInsertSlackUsersParams: ", batchInsertSlackUsersParams);
+  //console.log("batchInsertSlackUsersParams: ", batchInsertSlackUsersParams);
 
   let batchInsertNewSlackUserResult = await data.query(
     batchInsertNewSlackUserSql,
     batchInsertSlackUsersParams
   );
 
-  console.log("batchInsertNewSlackUserResult: ", batchInsertNewSlackUserResult);
+  //console.log("batchInsertNewSlackUserResult: ", batchInsertNewSlackUserResult);
 
   return { channelID: channelID, channelUUID: channelUUID };
 };
