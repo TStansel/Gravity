a8a56897c56e8371ed9821ef353770e334d462f3
Mason Spong <mason@Masons-MacBook-Air.local>
2022-01-14 13:53:38 -0700

    add nlpLambda.ts

---
+++ src/nlpLambda.ts
@@ -0,0 +1,15 @@
+import {
+  SQSHandler,
+  SQSEvent,
+  SQSBatchResponse,
+  SQSBatchItemFailure
+} from "aws-lambda";
+
+export const lambdaHandler: SQSHandler = async (
+  event: SQSEvent
+): Promise<void> => {
+  console.log(event);
+
+  
+  
+};
